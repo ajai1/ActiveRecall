2ee0e2539ba984796a93187659ab61651d725e80
ajai1 <emailtoajai@gmail.com>
2024-09-23 17:38:58 +0530

    About section

--- active_recall/src/components/Dashboard.jsx
+++ active_recall/src/components/Dashboard.jsx
@@ -25,6 +25,22 @@ export const Dashboard = () => {
       >
         Show Your Deck Of Cards
       </div>
+      <div className="dashboard_text_block">
+        <div>
+          <h3>This is a dev-in-progress application.</h3>
+          <p>Intition is to create an app similar to Memrise or Anki</p>
+        </div>
+        <div>
+          <ul>
+            <li>Spring Boot for Backend</li>
+            <li>Spring Security for Basic Auth</li>
+            <li>MongoDB for storage</li>
+            <li>React Quill package for the Rich Text Editor</li>
+            <li>React Router for routes</li>
+            <li>Context hooks for state management</li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
